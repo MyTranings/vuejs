f855b96e03ced8a205463ca3f44450ec3ae35711
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-04-22 16:10:14 +0300

    01 - Getting Started - Update to Vue JS

--- complete-guide/01-getting-started/app.js
+++ complete-guide/01-getting-started/app.js
@@ -1,17 +1,38 @@
 // Vanilla JS
 
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-function addGoal(event) {
-  event.preventDefault();
-  if (inputEl.value) {
-    const goal = document.createElement('li');
-    goal.textContent = inputEl.value;
-    listEl.appendChild(goal);
-    inputEl.value = '';
-  }
-}
+// function addGoal(event) {
+//   event.preventDefault();
+//   if (inputEl.value) {
+//     const goal = document.createElement('li');
+//     goal.textContent = inputEl.value;
+//     listEl.appendChild(goal);
+//     inputEl.value = '';
+//   }
+// }
 
-buttonEl.addEventListener('click', addGoal)
+// buttonEl.addEventListener('click', addGoal)
+
+// Vue JS
+
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    }
+  },
+  methods: {
+    addGoal() {
+      if (this.enteredValue.length > 0) {
+        this.goals.push(this.enteredValue);
+        this.enteredValue = '';
+      }
+    }
+  },
+
+}).mount('#app');
+
