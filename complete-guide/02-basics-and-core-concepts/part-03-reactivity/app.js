4d6914288775b2ddc6fe603766765674cb4edaaf
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-04 17:23:28 +0300

    02 - Basics and core - Part 03 - Watchers

--- complete-guide/02-basics-and-core-concepts/part-03-reactivity/app.js
+++ complete-guide/02-basics-and-core-concepts/part-03-reactivity/app.js
@@ -2,13 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      fullname: ''
     };
   },
-  computed: {
-    fullname() {
-      return this.name === '' ? this.name : this.name + ' ' + 'Georgiev';
+  watch: {
+    name(value) {
+      this.fullname = value === '' ? value : value + ' ' + this.lastName;
+    },
+    lastName(value) {
+      this.fullname = value === '' ? this.name : this.name + ' ' + value;
     }
+  },
+  computed: {
+    // fullname() {
+    // return this.name === '' ? this.name : this.name + ' ' + 'Georgiev';
+    // }
   },
   methods: {
     outputFullname() {
