25286a042e9583fff9b644d49a29539a2fd90cc4
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-04 17:24:48 +0300

    02 - Basics and core -  Part 03 - Computed instead of watchers

--- complete-guide/02-basics-and-core-concepts/part-03-reactivity/app.js
+++ complete-guide/02-basics-and-core-concepts/part-03-reactivity/app.js
@@ -4,21 +4,24 @@ const app = Vue.createApp({
       counter: 0,
       name: '',
       lastName: '',
-      fullname: ''
+      // fullname: ''
     };
   },
   watch: {
-    name(value) {
-      this.fullname = value === '' ? value : value + ' ' + this.lastName;
-    },
-    lastName(value) {
-      this.fullname = value === '' ? this.name : this.name + ' ' + value;
-    }
+    // name(value) {
+    // this.fullname = value === '' ? value : value + ' ' + this.lastName;
+    // },
+    // lastName(value) {
+    // this.fullname = value === '' ? this.name : this.name + ' ' + value;
+    // }
   },
   computed: {
     // fullname() {
     // return this.name === '' ? this.name : this.name + ' ' + 'Georgiev';
     // }
+    fullname() {
+      return this.name + ' ' + this.lastName;
+    }
   },
   methods: {
     outputFullname() {
