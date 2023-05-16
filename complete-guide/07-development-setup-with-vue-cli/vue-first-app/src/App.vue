fe446a676fbaad928d509f878faf37a32340d53a
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-16 15:58:21 +0300

    07 - Vue Dev Env Setup - Initial component file

---
+++ complete-guide/07-development-setup-with-vue-cli/vue-first-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0123 45678 991",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
--- complete-guide/07-development-setup-with-vue-cli/vue-first-app/src/main.js
+++ complete-guide/07-development-setup-with-vue-cli/vue-first-app/src/main.js
@@ -1,3 +1,5 @@
 import { createApp } from 'vue'
+import App from './App';
 
-createApp({}).mount('#app')
+
+createApp(App).mount('#app')
