913ccd8165ddefad5ed99a34939187566f49bd3c
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-17 15:19:34 +0300

    08 - Component communition - Task 1 - done

---
+++ complete-guide/08-component-communication/08-task-pops-custom-events/src/components/ActiveUser.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user">
+    <h2>Name</h2>
+    <h3>Age</h3>
+  </div>
+</template>
+
+<style>
+.user {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+.user h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+.user h3 {
+  font-size: 1.8rem;
+  color: #58004d;
+  margin: 0 0 0.5rem 0;
+}
+</style>
+
+<script>
+export default {};
+</script>
---
+++ complete-guide/08-component-communication/08-task-pops-custom-events/src/components/UserData.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </div>
+    <div>
+      <label for="age">Age</label>
+      <input type="number" name="age" id="age" />
+    </div>
+    <button>Add User</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+label {
+  font-weight: bold;
+  margin-right: 1rem;
+  display: inline-block;
+}
+form div {
+  margin: 1rem 0;
+}
+</style>
