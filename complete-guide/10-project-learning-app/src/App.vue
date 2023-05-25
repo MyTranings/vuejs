4b00c952928f8b6c63bcd26146444777af7c7990
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-25 17:57:11 +0300

    10 - Course Project: The Learning Resources App - Showing the resources list

---
+++ complete-guide/10-project-learning-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <TheResourcesList></TheResourcesList>
+</template>
+
+<style></style>
+
+<script>
+import TheResourcesList from "./components/TheResourcesList.vue";
+
+export default {
+  components: { TheResourcesList },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn google",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ complete-guide/10-project-learning-app/src/components/TheResourcesList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <TheResourceCard v-for="resource in storedResources" :key="resource.id">
+      <h2>{{ resource.title }}</h2>
+      <p>{{ resource.description }}</p>
+      <a :href="resource.link">View Resource</a>
+    </TheResourceCard>
+  </ul>
+</template>
+
+<script>
+import TheResourceCard from "./TheResourceCard.vue";
+
+export default {
+  components: { TheResourceCard },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn google",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
