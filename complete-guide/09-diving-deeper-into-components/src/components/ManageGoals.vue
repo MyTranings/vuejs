f2799bfa37ac4eed84ff06f80f7c828069af09f1
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-25 17:00:00 +0300

    09 - Diving deeper into components - Teleporting problem

--- complete-guide/09-diving-deeper-into-components/src/components/ManageGoals.vue
+++ complete-guide/09-diving-deeper-into-components/src/components/ManageGoals.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Error</h2>
+      <p>Enter some text</p>
+      <button @click="confirmError">Ok</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
