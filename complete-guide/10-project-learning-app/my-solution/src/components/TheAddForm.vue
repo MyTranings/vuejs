85f6844b6edcb79ab2e424d9c82a24d5ff8174fd
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-26 10:30:11 +0300

    10 - Course Project: The Learning Resources App - Implement v-model for custom component

--- complete-guide/10-project-learning-app/my-solution/src/components/TheAddForm.vue
+++ complete-guide/10-project-learning-app/my-solution/src/components/TheAddForm.vue
@@ -1,15 +1,46 @@
 <template>
-  <form>
-    <TheField type="text" label="Title" name="title"></TheField>
-    <TheField type="textarea" label="Description" name="description"></TheField>
-    <TheField type="text" label="Link" name="link"></TheField>
+  <form @submit.prevent="addResource">
+    <TheField type="text" label="Title" name="title" v-model="title"></TheField>
+    <TheField
+      type="textarea"
+      label="Description"
+      name="description"
+      v-model="description"
+    ></TheField>
+    <TheField type="text" label="Link" name="link" v-model="link"></TheField>
+    <TheButton>Add Resource</TheButton>
   </form>
 </template>
 
 <script>
 import TheField from "./TheField.vue";
+import TheButton from "./TheButton.vue";
 
 export default {
-  components: { TheField },
+  components: { TheField, TheButton },
+  data() {
+    return {
+      title: "",
+      description: "",
+      link: "",
+    };
+  },
+  methods: {
+    addResource() {
+      if (this.title === "" || this.description === "" || this.link === "") {
+        return false;
+      }
+      const newResource = {
+        id: this.title.replace(" ", "-").toLowerCase(),
+        title: this.title,
+        description: this.description,
+        link: this.link,
+      };
+      this.$emit("add-new-resource", newResource);
+      this.title = "";
+      this.description = "";
+      this.link = "";
+    },
+  },
 };
 </script>
--- complete-guide/10-project-learning-app/my-solution/src/components/TheField.vue
+++ complete-guide/10-project-learning-app/my-solution/src/components/TheField.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="field-wrapper">
     <label :for="name">{{ label }}</label>
-    <textarea v-if="type === 'textarea'" :name="name" :id="name"></textarea>
-    <input v-else :name="name" :id="name" type="text" />
+    <textarea
+      v-if="type === 'textarea'"
+      :name="name"
+      :id="name"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    ></textarea>
+    <input
+      v-else
+      :name="name"
+      :id="name"
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
   </div>
 </template>
 
 <script>
 export default {
   props: {
+    modelValue: {
+      required: true,
+    },
     type: {
       type: String,
       required: true,
@@ -22,5 +38,6 @@ export default {
       required: true,
     },
   },
+  emits: ["update:modelValue"],
 };
 </script>
