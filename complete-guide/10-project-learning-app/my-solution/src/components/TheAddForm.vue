c329f050a74e8025e01954b345b3364fef7bfc8b
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-26 12:23:39 +0300

    10 - Course Project: The Learning Resources App - Implement diablog if form is invalid

--- complete-guide/10-project-learning-app/my-solution/src/components/TheAddForm.vue
+++ complete-guide/10-project-learning-app/my-solution/src/components/TheAddForm.vue
@@ -1,45 +1,76 @@
 <template>
   <form @submit.prevent="addResource">
-    <TheField type="text" label="Title" name="title" v-model="title"></TheField>
+    <TheField
+      type="text"
+      label="Title"
+      name="title"
+      v-model="newResource.title"
+    ></TheField>
     <TheField
       type="textarea"
       label="Description"
       name="description"
-      v-model="description"
+      v-model="newResource.description"
     ></TheField>
-    <TheField type="text" label="Link" name="link" v-model="link"></TheField>
+    <TheField
+      type="text"
+      label="Link"
+      name="link"
+      v-model="newResource.link"
+    ></TheField>
     <TheButton>Add Resource</TheButton>
+    <teleport to="body">
+      <TheDialog v-if="!isValid" @close-modal="closeModal">
+        <p>Unfortunately, at least one input is invalid.</p>
+        <p>
+          Please check all inputs and make sure you enter at least a few
+          characters into each
+        </p>
+      </TheDialog>
+    </teleport>
   </form>
 </template>
 
 <script>
 import TheField from "./TheField.vue";
 import TheButton from "./TheButton.vue";
+import TheDialog from "./TheDialog.vue";
 
 export default {
-  components: { TheField, TheButton },
+  components: { TheField, TheButton, TheDialog },
   data() {
     return {
-      title: "",
-      description: "",
-      link: "",
+      isValid: true,
+      newResource: {
+        title: "",
+        description: "",
+        link: "",
+      },
     };
   },
   methods: {
     addResource() {
-      if (this.title === "" || this.description === "" || this.link === "") {
-        return false;
+      if (
+        this.newResource.title === "" ||
+        this.newResource.description === "" ||
+        this.newResource.link === ""
+      ) {
+        this.isValid = false;
+      } else {
+        const newResource = {
+          id: this.newResource.title.replace(" ", "-").toLowerCase(),
+          title: this.newResource.title,
+          description: this.newResource.description,
+          link: this.newResource.link,
+        };
+        this.$emit("add-new-resource", newResource);
+        this.title = "";
+        this.description = "";
+        this.link = "";
       }
-      const newResource = {
-        id: this.title.replace(" ", "-").toLowerCase(),
-        title: this.title,
-        description: this.description,
-        link: this.link,
-      };
-      this.$emit("add-new-resource", newResource);
-      this.title = "";
-      this.description = "";
-      this.link = "";
+    },
+    closeModal() {
+      this.isValid = true;
     },
   },
 };
---
+++ complete-guide/10-project-learning-app/my-solution/src/components/TheDialog.vue
@@ -0,0 +1,15 @@
+<template>
+  <dialog open>
+    <slot></slot>
+    <TheButton @click="$emit('close-modal')">Okay</TheButton>
+  </dialog>
+</template>
+
+<script>
+import TheButton from "./TheButton.vue";
+
+export default {
+  components: { TheButton },
+  methods: {},
+};
+</script>
