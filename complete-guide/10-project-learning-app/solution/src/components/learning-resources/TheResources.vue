a60abdfd834323511ce183dbe9ac9df7943228f0
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-05-26 16:27:30 +0300

    10 - Course Project: The Learning Resources App - Course solution - Add resource logic

--- complete-guide/10-project-learning-app/solution/src/components/learning-resources/TheResources.vue
+++ complete-guide/10-project-learning-app/solution/src/components/learning-resources/TheResources.vue
@@ -11,7 +11,9 @@
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -42,21 +44,30 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
+      addResource: this.addResource,
     };
   },
   computed: {
     storedResButtonMode() {
-      console.log(this.selectedTab === "stored-resources" ? null : "flat");
       return this.selectedTab === "stored-resources" ? null : "flat";
     },
     addResButtonMode() {
-      console.log(this.selectedTab === "add-resource" ? null : "flat");
       return this.selectedTab === "add-resource" ? null : "flat";
     },
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = "stored-resources";
     },
   },
 };
