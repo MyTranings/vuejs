cacbb617bf92121f769d13a25a786873f36fdf06
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-02 11:43:46 +0300

    13 - Routing: Muti-Page Sinle Page App - Add Routes

--- complete-guide/13-routing-muti-page-spa/src/main.js
+++ complete-guide/13-routing-muti-page-spa/src/main.js
@@ -1,7 +1,26 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams', // our-domain.com/teams => TeamsList
+      component: TeamsList
+    },
+    {
+      path: '/users', // our-domain.com/users => UsersList
+      component: UsersList
+    }
+  ]
+});
 
 const app = createApp(App)
+
+app.use(router);
 
 app.mount('#app');
