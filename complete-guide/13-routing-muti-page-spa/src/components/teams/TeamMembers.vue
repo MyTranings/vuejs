ff7f1104f94ee49d6786f4ffc7a98d5298ac447b
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-05 12:52:47 +0300

    13 - Routing: Muti-Page Sinle Page App - Update params data with watcher

--- complete-guide/13-routing-muti-page-spa/src/components/teams/TeamMembers.vue
+++ complete-guide/13-routing-muti-page-spa/src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to Team 2</router-link>
   </section>
 </template>
 
@@ -26,20 +27,30 @@ export default {
       members: [],
     };
   },
+  methods: {
+    loadTeamMembers(route) {
+      const teamId = route.params.teamId;
+      const selectedTeam = this.teams.find((team) => team.id === teamId);
+      const members = selectedTeam.members;
+      const selectedMembers = [];
+
+      for (const member of members) {
+        selectedMembers.push(this.users.find((user) => user.id === member));
+      }
+
+      this.members = selectedMembers;
+      console.log(selectedTeam);
+      this.teamName = selectedTeam.name;
+    },
+  },
   created() {
-    // this.$route.path();
-    const teamId = this.$route.params.teamId;
-    const selectedTeam = this.teams.find((team) => team.id === teamId);
-    const members = selectedTeam.members;
-    const selectedMembers = [];
-
-    for (const member of members) {
-      selectedMembers.push(this.users.find((user) => user.id === member));
-    }
-
-    this.members = selectedMembers;
-    console.log(selectedTeam);
-    this.teamName = selectedTeam.name;
+    // this.$route.path(); // /teams/t1
+    this.loadTeamMembers(this.$route);
+  },
+  watch: {
+    $route(newRoute) {
+      this.loadTeamMembers(newRoute);
+    },
   },
 };
 </script>
