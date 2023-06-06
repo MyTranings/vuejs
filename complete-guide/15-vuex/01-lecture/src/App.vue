55117813d05aa67ec805ed1c683d4cc13736cfc7
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-06 12:32:50 +0300

    15 Vuex - Using vuex mutations

--- complete-guide/15-vuex/01-lecture/src/App.vue
+++ complete-guide/15-vuex/01-lecture/src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <base-container title="Vuex">
-    <!-- Variant 1 -->
-    <!-- <h3>{{ count }}</h3> -->
-    <!-- Variant 2 -->
     <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from "./components/BaseContainer.vue";
-// Variant 2
 import TheCounter from "./components/TheCounter.vue";
+import ChangeCounter from "./components/ChangeCounter.vue";
 
 export default {
   components: {
     BaseContainer,
-    TheCounter, // Variant 2
-  },
-  computed: {
-    // Variant 1
-    // count() {
-    // return this.$store.state.counter;
-    // },
+    TheCounter,
+    ChangeCounter,
   },
   methods: {
     addOne() {
-      this.$store.state.counter += 1;
+      this.$store.commit("increment");
     },
   },
 };
