af6eca1fccd66c88f8b53bbe7cf914c097bf43e3
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-06 17:31:26 +0300

    15 Vuex - [WIP] My solution to export all data manipulation to vuex

---
+++ complete-guide/15-vuex/02-challenge/src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+
+    }
+  },
+  mutations: rootMutations,
+  actions: rootActions,
+  getters: rootGetters,
+})
+
+export default store;
