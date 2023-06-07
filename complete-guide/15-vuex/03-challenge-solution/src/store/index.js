96c4f458a63b3cd70934a347617c174e6e75f9a4
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-07 14:57:33 +0300

    15 Vuex Challenge Solution - Move is logged in logic to vuex store

--- complete-guide/15-vuex/03-challenge-solution/src/store/index.js
+++ complete-guide/15-vuex/03-challenge-solution/src/store/index.js
@@ -9,7 +9,30 @@ const store = createStore({
     cart: cartModule
   },
   state() {
-    return {}
+    return {
+      isLoggedIn: false
+    }
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit('login');
+    },
+    logout(context) {
+      context.commit('logout');
+    }
+  },
+  getters: {
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    }
   }
 })
 
