bae0ca01e13277b880278d9ef2dd8c160803c3e2
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-16 15:43:05 +0300

    16. Coach Finder - Solution - Add Vuex - Configure store for coaches and requests - Connect store and list coaches

--- complete-guide/16-coach-finder/02-solution/src/pages/coaches/CoachesList.vue
+++ complete-guide/16-coach-finder/02-solution/src/pages/coaches/CoachesList.vue
@@ -5,13 +5,27 @@
       <button>Refresh</button>
       <router-link to="/register">Register</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
     <router-link to=""></router-link>
   </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
 
 <style></style>
