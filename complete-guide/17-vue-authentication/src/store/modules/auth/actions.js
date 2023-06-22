e55d06076dea782a638b47932f120343004d86d7
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-22 10:44:55 +0300

    17. Vue Authentication - Add firebase sign up logic

--- complete-guide/17-vue-authentication/src/store/modules/auth/actions.js
+++ complete-guide/17-vue-authentication/src/store/modules/auth/actions.js
@@ -1,1 +1,26 @@
-export default {}
+export default {
+  login() { },
+  async signup(context, payload) {
+    console.log(payload)
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBx-muI5MANa_kQOvjJhSp6l0AiXVOG7Zs', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    })
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(response.message || 'Failed to authenticate')
+      throw error;
+    }
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    })
+  }
+}
