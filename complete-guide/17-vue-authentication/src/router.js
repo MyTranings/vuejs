4a21f25c84e6fc8350f64248cbd1732f973132be
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-22 11:41:00 +0300

    17. Vue Authentication - Add global navigation guard

--- complete-guide/17-vue-authentication/src/router.js
+++ complete-guide/17-vue-authentication/src/router.js
@@ -8,6 +8,8 @@ import RequestsReceived from './pages/requests/RequestsReceived.vue'
 import UserAuth from './pages/auth/UserAuth.vue'
 import NotFound from './pages/NotFound.vue'
 
+import store from './store/index.js';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -32,21 +34,40 @@ const router = createRouter({
     },
     {
       path: '/register',
-      component: CoachRegistration
+      component: CoachRegistration,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/requests',
-      component: RequestsReceived
+      component: RequestsReceived,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/auth',
-      component: UserAuth
+      component: UserAuth,
+      meta: {
+        requiresUnauth: true
+      }
     },
     {
       path: '/:notFound(.*)',
       component: NotFound
     },
   ],
+})
+
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
 })
 
 export default router;
