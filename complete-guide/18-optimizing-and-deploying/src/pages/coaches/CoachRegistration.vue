aaa9dc2091f107c85fe3719513a739f316e6e044
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-22 12:43:41 +0300

    18. Optimizing and Deploying -

---
+++ complete-guide/18-optimizing-and-deploying/src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,56 @@
+<template>
+  <section>
+    <base-spinner v-show="isLoading"></base-spinner>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from "./../../components/coaches/CoachForm.vue";
+
+export default {
+  components: { CoachForm },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    async saveData(data) {
+      this.isLoading = true;
+
+      await this.$store.dispatch("coaches/registerCoach", data);
+
+      this.isLoading = false;
+
+      this.$router.replace("/coaches"); // no back
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
