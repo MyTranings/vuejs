61ab387f33a96e135ded12961a0900744588b54d
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-23 15:54:05 +0300

    19. Composition API - Working with lifecycle methods

--- complete-guide/19-composition-api/lecture/src/components/UserData.vue
+++ complete-guide/19-composition-api/lecture/src/components/UserData.vue
@@ -4,7 +4,16 @@
 </template>
 
 <script>
-import { computed, inject } from "vue";
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
 
 export default {
   props: ["first-name", "last-name"],
@@ -21,6 +30,30 @@ export default {
     context.emit("save-data", 1); // this.$emit('save-data', 1)
 
     const age = inject("userAge");
+
+    onBeforeMount(function () {
+      console.log("onBeforeMount");
+    });
+
+    onMounted(function () {
+      console.log("onMounted");
+    });
+
+    onBeforeUpdate(function () {
+      console.log("onBeforeUpdate");
+    });
+
+    onUpdated(function () {
+      console.log("onUpdated");
+    });
+
+    onBeforeUnmount(function () {
+      console.log("onBeforeUnmount");
+    });
+
+    onUnmounted(function () {
+      console.log("onUnmounted");
+    });
 
     return {
       userName: uName,
