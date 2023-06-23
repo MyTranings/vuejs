56bc1d754f5be9a4b5be02d860ab981f85fbccd3
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-23 14:26:02 +0300

    19. Composition API - Assignment 1 - Task 1 and 2 solved

--- complete-guide/19-composition-api/assignment-1-data-and-functions/src/App.vue
+++ complete-guide/19-composition-api/assignment-1-data-and-functions/src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-show="isGoalVisible">{{ myGoal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toogleGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +13,26 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
-  
-}
+  data() {
+    return {};
+  },
+  setup() {
+    const myGoal = ref("My Goals is understand Vue");
+    const isGoalVisible = ref(true);
+
+    function toogleGoal() {
+      isGoalVisible.value = !isGoalVisible.value;
+    }
+
+    return {
+      myGoal,
+      isGoalVisible,
+      toogleGoal,
+    };
+  },
+};
 </script>
 
 <style>
