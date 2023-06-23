de903ef48f64ef037e63dff13c13b548fed3d65a
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-23 16:13:49 +0300

    19. Composition API - Exercise - Update Base Search to Composition API

--- complete-guide/19-composition-api/exercise/src/components/UI/BaseSearch-original.vue
+++ complete-guide/19-composition-api/exercise/src/components/UI/BaseSearch-original.vue
@@ -13,13 +13,10 @@
 export default {
   props: ["searchTerm"],
   emits: ["search"],
-  setup(_, context) {
-    function search(event) {
-      context.emit("search", event.target.value);
-    }
-    return {
-      search,
-    };
+  methods: {
+    search(event) {
+      this.$emit("search", event.target.value);
+    },
   },
 };
 </script>
