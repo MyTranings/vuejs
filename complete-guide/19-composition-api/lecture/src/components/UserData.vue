d713b517b5cdf64c25ed0d0d9ec7097d1a2756e9
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-23 15:43:20 +0300

    19. Composition API - Working with context attrs, slots and emit

--- complete-guide/19-composition-api/lecture/src/components/UserData.vue
+++ complete-guide/19-composition-api/lecture/src/components/UserData.vue
@@ -8,10 +8,18 @@ import { computed } from "vue";
 
 export default {
   props: ["first-name", "last-name", "age"],
-  setup(props) {
+  setup(props, context) {
     const uName = computed(function () {
       return props.firstName + " " + props.lastName;
     });
+
+    console.log(context);
+
+    context.attrs; // pass through props
+    context.slots; // template slots
+    // Emit
+    context.emit("save-data", 1); // this.$emit('save-data', 1)
+
     return {
       userName: uName,
     };
