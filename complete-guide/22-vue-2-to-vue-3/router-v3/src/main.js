36e3d54bbeb9605d9be1a877fda689bfa969118b
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-06-27 11:17:33 +0300

    22. Vue 2 to Vue 3 - Changes in Vue Router for Vue 3

--- complete-guide/22-vue-2-to-vue-3/router-v3/src/main.js
+++ complete-guide/22-vue-2-to-vue-3/router-v3/src/main.js
@@ -1,7 +1,26 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import WelcomeScreen from './pages/WelcomeScreen.vue';
+import UsersList from './pages/UsersList.vue';
 
 // Add router!
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: WelcomeScreen },
+    { path: '/users', component: UsersList },
+  ],
+})
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+
+// No inital transition
+router.isReady().then(() => {
+  app.mount('#app');
+})
+
